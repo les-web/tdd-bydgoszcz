6c4c556463221b7368b9ed6270d6683befc11d68
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-08-22 11:16:05 +0200

    tdd 1

--- src/test/java/a_junit/CalculatorTest.java
+++ src/test/java/a_junit/CalculatorTest.java
@@ -1,12 +1,39 @@
 package a_junit;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class CalculatorTest {
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("BEFORE ALL");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("AFTER ALL");
+    }
+
     @Test
+    //  @Disabled
+
     public void test() {
 // Arrange
         Calculator calculator = new Calculator();
-        
+// Act
+        Integer sum = calculator.add(2, 2);
+        // Assert
+
+        Assertions.assertEquals(4, sum);
+    }
+
+    @Test
+    public void test2() {
+// Arrange
+        Calculator calculator = new Calculator();
+// Act
+        Integer sum = calculator.add(1, 1);
+        // Assert
+
+        Assertions.assertEquals(2, sum);
     }
 }
