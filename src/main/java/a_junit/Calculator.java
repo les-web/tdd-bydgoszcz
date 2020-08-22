e8bf6caf6669b3ce4a76f99fe9a31a9c125939f4
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-08-22 10:24:55 +0200

    tdd 1

---
+++ src/main/java/a_junit/Calculator.java
@@ -0,0 +1,8 @@
+package a_junit;
+
+public class Calculator {
+
+    public Integer add(Integer a, Integer b) {
+        return a+b;
+    }
+}
---
+++ src/test/java/a_junit/CalculatorTest.java
@@ -0,0 +1,12 @@
+package a_junit;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    public void test() {
+// Arrange
+        Calculator calculator = new Calculator();
+        
+    }
+}
