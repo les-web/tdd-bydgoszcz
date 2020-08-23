9b54e0ade306d6e9964a398436923630728bef16
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-08-23 15:45:22 +0200

    PowerMockTest 2

--- src/test/java/f_powermock/PowerMockTest.java
+++ src/test/java/f_powermock/PowerMockTest.java
@@ -8,6 +8,8 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import static org.powermock.api.mockito.PowerMockito.verifyNew;
+
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(UserValidator.class)
 
@@ -19,5 +21,21 @@ public class PowerMockTest {
 
         UserValidator userValidator1 = new UserValidator();
         Assertions.assertThat(userValidator1).isEqualTo(userValidator);
+        verifyNew(UserValidator.class).withNoArguments();
+    }
+
+    @Test
+    public void test2() {
+        UserValidator userValidator = PowerMockito.mock(UserValidator.class);
+        PowerMockito.when(userValidator.validateFinal()).thenReturn(false);
+        boolean valid = userValidator.validateFinal();
+        Assertions.assertThat(valid).isFalse();
+    }
+    @Test
+    public void test3() {
+        PowerMockito.mockStatic(UserValidator.class);
+        PowerMockito.when(UserValidator.validateStatic()).thenReturn(false);
+        boolean valid = UserValidator.validateStatic();
+        Assertions.assertThat(valid).isFalse();
     }
 }
--- src/test/java/f_powermock/UserValidator.java
+++ src/test/java/f_powermock/UserValidator.java
@@ -3,4 +3,10 @@ package f_powermock;
 public class UserValidator {
     UserValidator() {}
 
+    final boolean validateFinal ()
+    {return true;}
+
+    static boolean validateStatic()
+    {return true;}
+
 }
