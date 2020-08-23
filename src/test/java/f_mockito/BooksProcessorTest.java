8249d6ae419208c52e4d6074de90869241dd022f
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-08-23 13:39:12 +0200

    mockito exercises 1

---
+++ src/test/java/f_mockito/BooksProcessorTest.java
@@ -0,0 +1,14 @@
+package f_mockito;
+/*
+# Zadanie
+
+        1. Utwórz klasę z testami `BooksProcessorTest`.
+        2. Przetestuj metody klasy `BooksProcessor`. Rozważ różne przypadki testowe.
+        3. Przy tworzeniu obiektu typu `BooksProcessor` pamiętaj o wstrzyknięciu
+        atrapy obiektu typu `Database`.
+        Na początku użyj standardowego sposobu tworzenia mocków.
+        4. Kiedy skończysz zmień kod w taki sposób
+        aby wykorzystywał adnotacje `@Mock` i `@InjectMocks`.
+*/
+public class BooksProcessorTest {
+}
